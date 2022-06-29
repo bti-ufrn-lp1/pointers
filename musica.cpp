a98d647caf88c18174f5361727dc0deb2bb6d72c
Everton Cavalcante <evertonranielly@gmail.com>
2022-06-29 09:19:29 -0300

    Atualização nos exemplos

--- musica.cpp
+++ musica.cpp
@@ -2,25 +2,25 @@
 using std::make_unique;
 using std::unique_ptr;
 
-#include <vector>
-using std::vector;
-
 #include <iostream>
 using std::cout;
 using std::endl;
 
+#include <utility>
+using std::move;
+
 #include "musica.h"
 
-void printPosicaoYT(int posicao, unique_ptr<Musica> musica) {
+void printPosicaoYT(int posicao, unique_ptr<Musica> &musica) {
     cout << "Top #" << posicao << " on YouTube: " << musica->getTitulo() << endl;
 }
 
 int main() {
     unique_ptr<Musica> top1yt(new Musica("Baby Shark Dance"));
-    printPosicaoYT(1, top1yt);
-
     unique_ptr<Musica> top2yt = make_unique<Musica>("Despacito");
-    printPosicaoYT(2, top2yt);
 
+    printPosicaoYT(1, top1yt);
+    printPosicaoYT(2, top2yt);
+    
     return 0;
 }
--- vetor.cpp
+++ vetor.cpp
@@ -2,14 +2,17 @@
 using std::cout;
 using std::endl;
 
+#include <memory>
+using std::unique_ptr;
+
+#include <utility>
+using std::make_unique;
+
 #define SIZE 20
 
 int main() {
-    int* vetor = new int[SIZE];
-    for (int i = 0; i < SIZE; i++) {
-        vetor[i] = i+1;
-    }
-
+    unique_ptr<int[]> vetor = make_unique<int[]>(SIZE);
+    for (int i = 0; i < SIZE; i++) vetor[i] = i + 1;
     for (int i = 0; i < SIZE; i++) {
         cout << vetor[i] << " ";
     }
